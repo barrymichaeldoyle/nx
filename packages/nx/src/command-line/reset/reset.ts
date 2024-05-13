883800b0b04695716b0230fca96fb7846dfc8312
Colum Ferry <cferry09@gmail.com>
2024-05-13 13:53:09 +0100

    wip

--- packages/nx/src/command-line/reset/reset.ts
+++ packages/nx/src/command-line/reset/reset.ts
@@ -5,6 +5,8 @@ import {
   projectGraphCacheDirectory,
 } from '../../utils/cache-directory';
 import { output } from '../../utils/output';
+import { join } from 'path';
+import { lstatSync, readdirSync } from 'fs';
 
 export async function resetHandler() {
   output.note({
@@ -13,7 +15,22 @@ export async function resetHandler() {
   });
   await daemonClient.stop();
   output.log({ title: 'Daemon Server - Stopped' });
-  rmSync(cacheDir, { recursive: true, force: true });
+  await new Promise((resolve) => setTimeout(resolve, 1500));
+  const nativeFile = `20.0.0-nx.win32-x64-msvc.node`;
+  // rmSync(cacheDir, { recursive: true, force: true });
+  const cacheDirFiles = readdirSync(cacheDir).map((filePath) =>
+    join(cacheDir, filePath)
+  );
+  for (const file of cacheDirFiles) {
+    if (lstatSync(file).isDirectory()) {
+      rmSync(file, { recursive: true, force: true });
+    } else {
+      if (file.endsWith('.node')) {
+        continue;
+      }
+      rmSync(file);
+    }
+  }
   if (projectGraphCacheDirectory !== cacheDir) {
     rmSync(projectGraphCacheDirectory, { recursive: true, force: true });
   }
